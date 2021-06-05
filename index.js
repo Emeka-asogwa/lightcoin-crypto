1a3dcadecea7fbe6a1c9502d0477e7b2db66a65b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-05 14:45:13 +0000

    ✍🏽 updated the lightcoin-crypto with final solution

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-let balance = 500.00;
+//let balance = 500.00;
 class Account {
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+  get balance() {
+    let balance = 0;
+    for (let k of this.transactions) {
+      balance += k.value;
+
+    }
+    return balance;
+
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 }
 
@@ -12,7 +24,10 @@ class Transaction {
     this.account = account;
   }
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 }
 
@@ -21,11 +36,17 @@ class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 class Deposit extends Transaction {
   get value() {
     return this.amount;
+  }
+  isAllowed() {
+    return true;
   }
 }
 
@@ -39,17 +60,22 @@ class Deposit extends Transaction {
 const myAccount = new Account("billybob");
 console.log(`The username ${myAccount.username} has a starting Balance:`, myAccount.balance);
 
-const t3 = new Deposit(120.00, myAccount);
-t3.commit();
-// console.log('Transaction 3:', t3)
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result: ',t1.commit());
+console.log('Account Balance:', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t3 = new Deposit(9.99, myAccount);
+console.log('Commit result:',t3.commit());
+console.log('Account Balance: ', myAccount.balance);
 
 
-t1 = new Withdrawal(50.00, myAccount);
-t1.commit();
-console.log('Ending Balance:', myAccount.balance);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t2 = new Withdrawal(9.99, myAccount);
+console.log('Commit result', t2.commit());
+console.log('Ending Account Balance: ', myAccount.balance);
 
-// t2 = new Withdrawal(9.99);
-// t2.commit();
-// console.log('Transaction 2:', t2);
+console.log("Seems I am currently out of cash");
 
-// console.log('Balance:', balance);
+console.log('Account Transaction History: ', myAccount.transactions);
